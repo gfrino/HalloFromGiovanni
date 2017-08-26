a097c19839d39885482bad2526ceb0696f283a5d
Giovanni Frino <gfrino@me.com>
2017-08-26 18:56:51 +0200

    Initial Commit

---
+++ HalloFromGiovanni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HalloFromGiovanni
+//
+//  Created by Giovanni Frino on 26.08.17.
+//  Copyright © 2017 Giovanni Frino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
